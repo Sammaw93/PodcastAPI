a25f866b26df40c94898f7c98588a9fd2be9a1e5
Sammawhinney93 <sam.852@hotmail.co.uk>
2018-08-01 18:36:31 +0100

    added JWT

--- app/User.php
+++ app/User.php
@@ -26,4 +26,35 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * A user can have as many episodes as possible
+     *
+     * @return Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function episodes()
+    {
+        return $this->hasMany(Episode::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
 }
