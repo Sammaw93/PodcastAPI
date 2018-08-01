6916c3bd17c968e6975c9e9631d7b48095d15e9b
Sammawhinney93 <sam.852@hotmail.co.uk>
2018-08-01 16:28:56 +0100

    added rating.php file

---
+++ app/Rating.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    /**
+     * Attributes that can be mass assigned
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'user_id', 'rating'];
+
+    /**
+     * A rating belongs to a episode
+     *
+     * @return Illuminate\Database\Eloquent\Relations\Relation
+     */
+
+    public function episode()
+    {
+        return $this->belongsTo(Episode::class);
+    }
+
+
+}
